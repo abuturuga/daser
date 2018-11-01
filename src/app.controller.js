938a6e39307aeaacbd7e7de47a2433c24bac44d9
Buturuga Alexandru <buturuga.alexandru.iulian@gmail.com>
2018-11-01 22:16:43 +0200

    [Panels] small changes, seed repetitive elements via javascript

--- src/app.controller.js
+++ src/app.controller.js
@@ -5,22 +5,38 @@ export class AppController {
   constructor() {
     this.toggleStructureBtn = document.querySelector('#structure-toggle-btn');
     this.togglePropertiesBtn = document.querySelector('#properties-toggle-btn');
-
     this.structurePanel = document.querySelector('.structure-panel');
+    this.tablePropsList = document.querySelector('.table-properties__columns__list');
   }
 
   init() {
-    this.initTogglePanels();
+    this.seedTableList();
+    this.seedTableTree();
   }
 
-  initTogglePanels() {
-    this.toggleStructureBtn.addEventListener('click', _ => {
-      (this.structurePanel.style.display === 'none')
-        ? this.structurePanel.style.display = 'block'
-        : this.structurePanel.style.display = 'none';
-    });
+  seedTableList() {
+    const $li = this.tablePropsList.querySelector('li');
+    Array(5).fill(0)
+      .map(_ => $li.cloneNode(true))
+      .forEach($element => {
+        this.tablePropsList.appendChild($element)
+      });
   }
 
+  seedTableTree() {
+    const $tableList = document.querySelector('.table-tree_list');
+    const $li = $tableList.querySelector('li');
+
+    Array(5).fill(0)
+      .map(_ => $li.cloneNode(true))
+      .forEach($element => {
+        $tableList.appendChild($element)
+      });
+  }
+
+  seedRefsList() {
+
+  }
 }
 
 
