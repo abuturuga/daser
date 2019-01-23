de9946d8608297a920b654259b4b5b7b70e109b3
Buturuga Alexandru <buturuga.alexandru.iulian@gmail.com>
2019-01-23 22:50:16 +0200

    [header-component] refactor

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,47 +1,65 @@
 import { BaseComponent } from '../../base.component';
 
+
 const COMPONENT_CLASS = 'header-component';
+const LOGO_CLASS = `${COMPONENT_CLASS}__logo`;
+const MOBILE_BUTTON = `${COMPONENT_CLASS}__mobile-menu-btn`;
+
+const WORK_BAR = `${COMPONENT_CLASS}__work-bar`;
+const WORK_BAR_BUTTON = `${WORK_BAR}__button`;
+
 
 export class HeaderComponent extends BaseComponent {
 
   constructor() {
     super('div', COMPONENT_CLASS);
   }
- 
+  
+  renderButtonTemplate(name) {
+    return `<button class="${WORK_BAR_BUTTON}" data-type="${name}">
+      <i class="material-icons">${name}</i>
+    </button>`;
+  }
+
+  bindEventListeners() {
+    Array.prototype.forEach.call(this.$element.querySelectorAll(`.${WORK_BAR_BUTTON}`), ($button) => {
+      $button.addEventListener('click', event => {
+        const type = $button.dataset.type;
+        // TODO:
+      });
+    });
+  }
+
   getTemplate() {
+    const editButtons = ["undo", "redo", "call_made"]
+      .map(buttonName => this.renderButtonTemplate(buttonName))
+      .join('');
+
+    const addButtons = ["add_box", "keyboard_return", "note_add"]
+      .map(buttonName => this.renderButtonTemplate(buttonName))
+      .join('');
+
+    const saveButtons = ['cloud_upload', 'share', 'save']
+      .map(buttonName => this.renderButtonTemplate(buttonName))
+      .join('');
+
     return `
-      <span class="logo">DASER</span>
-      <button class="icon-btn navigation__mobile-menu-btn">
+      <span class="${LOGO_CLASS}">DASER</span>
+      <button class="icon-btn ${MOBILE_BUTTON}">
         <i class="material-icons">menu</i>
       </button>
 
-      <div class="work-bar">
-        <button class="work-bar__button"><i class="material-icons">undo</i></button>
-        <button class="work-bar__button"><i class="material-icons">redo</i></button>
-        <button class="work-bar__button"><i class="material-icons">call_made</i></button>
-        
-        <span class="work-bar__separator"></span>
-        
-        <button class="work-bar__button"><i class="material-icons">add_box</i></button>
-        <button class="work-bar__button"><i class="material-icons">keyboard_return</i></button>
-        <button class="work-bar__button"><i class="material-icons">note_add</i></button>
+      <div class="${WORK_BAR}">
+        ${ editButtons }
+        <span class="${WORK_BAR}__separator"></span>
+        ${ addButtons }
       </div>
 
       <div class="options-bar">
-        <button class="work-bar__button">
-          <i class="material-icons">cloud_upload</i>
-        </button>
-        <button class="work-bar__button">
-          <i class="material-icons">share</i>
-        </button>
-        <button class="work-bar__button">
-          <i class="material-icons">save</i>
-        </button>
+        ${ saveButtons }
       </div>
-      <button class="work-bar__button mobile-options-btn">
-        <i class="material-icons">more_vert</i>
-      </button>
+      ${ this.renderButtonTemplate('more_vert') }
     `
-  } 
+  }
 
 }
