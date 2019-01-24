acb18b52864ba0e0cbbc3b9dfc273ac5a5ad4710
Buturuga Alexandru <buturuga.alexandru.iulian@gmail.com>
2019-01-24 14:48:18 +0200

    [properties-panel] refactor

--- src/components/properties-panel/properties-panel.component.js
+++ src/components/properties-panel/properties-panel.component.js
@@ -1,8 +1,12 @@
 import { BaseComponent } from '../../base.component.js';
-
+import PubSub from '../../pubsub.js';
+import {
+  PanelHeaderComponent,
+  PanelContentComponent
+} from '../shared/index.js';
 
 const COMPONENT_CLASS = 'properties-panel-component';
-
+const PANEL_HEADER_CLASS = 'panel-header';
 
 export class PropertiesPanelComponent extends BaseComponent {
 
@@ -10,82 +14,90 @@ export class PropertiesPanelComponent extends BaseComponent {
     super('div', COMPONENT_CLASS);
   }
 
+  renderChildComponents() {
+    this.panelHeader = new PanelHeaderComponent();
+
+    this.attach(this.panelHeader.render({
+      title: 'Structure',
+      onClose: () => console.log('on close')
+    }), `.${PANEL_HEADER_CLASS}`);
+  }
+
+  getColumnsListTemplate() {
+    return `
+    <ul class="table-properties__columns__list">
+      <li>
+        <div class="input-form">
+          <label>Name</label>
+          <input type="text" placeholder="Column Name"/>
+        </div>
+        <div class="input-form">
+          <label>Type</label>
+          <input type="text" placeholder="Column Type"/>
+        </div>
+        <span class="input-checkbox">
+          <label><span>Not Null</span>
+            <i class="material-icons">check_box</i>
+          </label>
+        </span>
+        <span class="input-checkbox">
+          <label><span>PK</span>
+            <i class="material-icons">check_box</i>
+          </label>
+        </span>
+        <i class="material-icons">edit</i>
+        <i class="material-icons">delete</i>
+      </li>
+    </ul>
+    `;
+  }
+
   getTemplate() {
     return `
-      <header class="panel__header"></header>
+      <header class="${PANEL_HEADER_CLASS}"></header>
 
-    <div class="panel__content">
-      <div class="table-properties-panel">
-        <section class="panel__content__section">
-          <header class="panel__content__section__header">
-            <h3 class="panel__content__section__header__title">Meta</h3>
-          </header>
-          <div class="input-form">
-            <label>Name</label>
-            <input type="text" placeholder="Table name"/>
-          </div>
+      <div class="panel__content">
+        ${PanelContentComponent(
+          'Meta', `
+            <div class="input-form">
+              <label>Name</label>
+              <input type="text" placeholder="Table name"/>
+            </div>
 
-          <div class="input-form">
-            <label>Description</label>
-            <textarea rows="2" placeholder="Table description"></textarea>
-          </div>
-        </section>
+            <div class="input-form">
+              <label>Description</label>
+              <textarea rows="2" placeholder="Table description"></textarea>
+            </div>
+          `
+        )}
 
-        <section class="panel__content__section">
-          <header class="panel__content__section__header">
-            <h3 class="panel__content__section__header__title">Columns</h3>        
-          </header>
-      
-          <div class="table-properties__columns__header">
-            <label>Name</label>
-            <label>Type</label>
-            <label>NL</label>
-            <label>PK</label>
-          </div>
+        ${PanelContentComponent(
+          'Columns', `
+            <div class="table-properties__columns__header">
+              <label>Name</label>
+              <label>Type</label>
+              <label>NL</label>
+              <label>PK</label>
+            </div>
+            ${this.getColumnsListTemplate()}
+          `
+        )}
 
-          <ul class="table-properties__columns__list">
-            <li>
-              <div class="input-form">
-                <label>Name</label>
-                <input type="text" placeholder="Column Name"/>
-              </div>
-              <div class="input-form">
-                <label>Type</label>
-                <input type="text" placeholder="Column Type"/>
-              </div>
-              <span class="input-checkbox">
-                <label><span>Not Null</span>
-                  <i class="material-icons">check_box</i>
-                </label>
-              </span>
-              <span class="input-checkbox">
-                <label><span>PK</span>
-                  <i class="material-icons">check_box</i>
-                </label>
-              </span>
-              <i class="material-icons">edit</i>
-              <i class="material-icons">delete</i>
-            </li>
-          </ul>
-        </section>
+        ${PanelContentComponent(
+          'Aditional Scripts',
+          `
+            <div class="input-form">
+              <label>Before table creation</label>
+              <textarea placeholder="SQL code here..."></textarea>
+            </div>
 
-        <section class="panel__content__section">
-          <header class="panel__content__section__header">
-            <h3 class="panel__content__section__header__title">Aditional Scripts</h3>        
-          </header>
-
-          <div class="input-form">
-            <label>Before table creation</label>
-            <textarea placeholder="SQL code here..."></textarea>
-          </div>
-
-          <div class="input-form">
-            <label>After table creation</label>
-            <textarea placeholder="SQL code here..."></textarea>
-          </div>
-        </section>
+            <div class="input-form">
+              <label>After table creation</label>
+              <textarea placeholder="SQL code here..."></textarea>
+            </div>
+          `
+        )}
       </div>
-    </div>
     `
   }
 
